5a4ebf6f4ffe65bb137002a0453fa67e6a19c2fa
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-12-14 13:52:00 +0000

    HTBHFB-99 Updated run.sh to work with node apps

--- cd_scripts/run.sh
+++ cd_scripts/run.sh
@@ -14,18 +14,11 @@ check_variable_is_set(){
     fi
 }
 
-check_variable_is_set APP_URL
-check_variable_is_set MANIFEST_URL
 check_variable_is_set DEPLOY_SCRIPTS_URL
 check_variable_is_set DEPLOY_SCRIPT_VERSION
 check_variable_is_set BIN_DIR
 
-wget -q -O artefact.jar ${APP_URL}
-wget -q -O manifest.jar ${MANIFEST_URL}
-jar -xf manifest.jar
-
-export APP_PATH=artefact.jar
-export CF_SPACE=staging
+export BIN_DIR=$(readlink -f ${BIN_DIR})
 
 echo "Installing deploy scripts"
 if [[ ! -e ${BIN_DIR}/deploy_scripts_${DEPLOY_SCRIPT_VERSION} ]]; then
@@ -35,5 +28,34 @@ if [[ ! -e ${BIN_DIR}/deploy_scripts_${DEPLOY_SCRIPT_VERSION} ]]; then
     touch deploy_scripts_${DEPLOY_SCRIPT_VERSION}
     cd ..
 fi
+
+
+echo "Determining whether to deploy node or java application (Node if ZIP_URL is set: '$ZIP_URL')"
+if [[ ${ZIP_URL} ]]; then
+    echo "Deploying Node.js app from '${ZIP_URL}'"
+    # download and extract the archive
+    mkdir -p application && cd application
+    wget -q -O application.zip ${ZIP_URL} && unzip -o application.zip && rm application.zip
+    # the archive should have a single folder - step into it to get the path
+    cd *
+    export APP_PATH=$(pwd)
+    if [[ ! -e manifest.yml ]]; then
+        echo "Error - cannot find manifest file in ${APP_PATH}"
+        exit 1
+    fi
+else
+    echo "Deploying Java app from '${APP_URL}' using manifest from '${MANIFEST_URL}'"
+    check_variable_is_set APP_URL
+    check_variable_is_set MANIFEST_URL
+
+    wget -q -O artefact.jar ${APP_URL}
+    wget -q -O manifest.jar ${MANIFEST_URL}
+    # extract the manifest into the current directory
+    jar -xf manifest.jar
+    export APP_PATH=artefact.jar
+fi
+
+
+export CF_SPACE=staging
 
 /bin/bash ${BIN_DIR}/deploy.sh
