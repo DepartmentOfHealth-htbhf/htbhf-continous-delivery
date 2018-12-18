2bb0e78466c226d6aa56bb4c22087fc067a2ec08
David Wybourn <dwybourn@users.noreply.github.com>
2018-12-18 10:08:31 +0000

    HTBHF-22 Removed gh-pages from end of push command. (#22)
    * HTBHF-22 Removed gh-pages from end of push command.

--- cd_scripts/publish_test_results.sh
+++ cd_scripts/publish_test_results.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+export PERFORMANCE_TEST_RESULTS=${WORKING_DIR}/docs/performance_results
+
 git fetch
 git checkout gh-pages
-rm -r -f ${WORKING_DIR}/docs/*
+rm -r -f ${PERFORMANCE_TEST_RESULTS}
+mkdir -p ${PERFORMANCE_TEST_RESULTS}
 
 # move test results to docs directory
 for f in ${RESULTS_DIRECTORY}/*simulation*; do
-  mv ${f}/* ${WORKING_DIR}/docs/performance_results;
+  mv ${f}/* ${PERFORMANCE_TEST_RESULTS};
 done
 
 git add docs/
 git config --local user.email "travis@travis-ci.org"
 git config --local user.name "Travis CI"
 git commit -m "Publishing test results"
-git push https://${GH_WRITE_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git push https://${GH_WRITE_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 
 git checkout master
