b148cbb5065529101b8a5028b657e10691b3d680
Travis CI <travis@travis-ci.org>
2018-12-31 11:33:44 +0000

    Publishing test results

--- docs/performance_results/js/all_sessions.js
+++ docs/performance_results/js/all_sessions.js
@@ -3,7 +3,7 @@ allUsersData = {
 color: '#FF9D00',
 name: 'Active Users',
 data: [
-  [1545399455000,50],[1545399456000,50],[1545399457000,49]
+  [1546256012000,50],[1546256013000,50],[1546256014000,50]
 ],
 tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
     , zIndex: 20
