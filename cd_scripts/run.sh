f9e897036af7464f0fc849484c885a7c54d23461
Matthew Smith <msmith@equalexperts.com>
2019-01-15 10:48:49 +0000

    HTBHF-251 Updated run.sh to allow cd to be triggered manually (skipping the deploy step)

--- cd_scripts/run.sh
+++ cd_scripts/run.sh
@@ -15,36 +15,44 @@ export COMPATIBILITY_TESTS_DIR=${WORKING_DIR}/compatibility_tests
 
 source ${CD_SCRIPTS_DIR}/cd_functions.sh
 
-check_variable_is_set APP_VERSION
 check_variable_is_set PERF_TESTS_URL
 check_variable_is_set PERF_TESTS_VERSION
 check_variable_is_set GH_WRITE_TOKEN
 check_variable_is_set TRAVIS_REPO_SLUG
-check_variable_is_set GITHUB_REPO_SLUG
 check_variable_is_set APP_HOST_STAGING
 
-download_deploy_scripts
+if [ -z "$GITHUB_REPO_SLUG" ]; then
+    echo "GITHUB_REPO_SLUG is empty/not set - not deploying any app";
 
-# SCRIPT_DIR should be set by download_deploy_scripts
-check_variable_is_set SCRIPT_DIR
-
-echo "Determining whether to deploy node or java application (will be node if ZIP_URL is set: '$ZIP_URL')"
-if [[ ${ZIP_URL} ]]; then
-    echo "Deploying Node.js app from '${ZIP_URL}'"
-    prepare_node_app_for_deploy
 else
-    echo "Deploying Java app from '${APP_URL}' using manifest from '${MANIFEST_URL}'"
-    prepare_java_app_for_deploy
+    echo "Deploying $GITHUB_REPO_SLUG";
+    check_variable_is_set APP_NAME
+    check_variable_is_set APP_VERSION
+
+    download_deploy_scripts
+
+    # SCRIPT_DIR should be set by download_deploy_scripts
+    check_variable_is_set SCRIPT_DIR
+
+    echo "Determining whether to deploy node or java application (will be node if ZIP_URL is set: '$ZIP_URL')"
+    if [[ ${ZIP_URL} ]]; then
+        echo "Deploying Node.js app from '${ZIP_URL}'"
+        prepare_node_app_for_deploy
+    else
+        echo "Deploying Java app from '${APP_URL}' using manifest from '${MANIFEST_URL}'"
+        prepare_java_app_for_deploy
+    fi
+
+    export CF_SPACE=staging
+    export SMOKE_TESTS=${CD_SCRIPTS_DIR}/deploy_smoke_test.sh
+    export APP_HOST=${APP_HOST_STAGING}
+
+    source ${SCRIPT_DIR}/deploy.sh
+    check_exit_status $? "Deployment"
+
+    cd ${WORKING_DIR}
 fi
 
-export CF_SPACE=staging
-export SMOKE_TESTS=${CD_SCRIPTS_DIR}/deploy_smoke_test.sh
-export APP_HOST=${APP_HOST_STAGING}
-
-source ${SCRIPT_DIR}/deploy.sh
-check_exit_status $? "Deployment"
-
-cd ${WORKING_DIR}
 
 download_compatibility_tests
 
@@ -64,7 +72,12 @@ source ${PERF_TESTS_DIR}/run_performance_tests.sh
 echo "Publishing test results"
 source ${CD_SCRIPTS_DIR}/publish_test_results.sh
 
+if [ -z "$GITHUB_REPO_SLUG" ]; then
+    echo "Staging build successful";
 
-echo "Staging build successful - Creating a release in GitHub for ${GITHUB_REPO_SLUG}"
-body="{\"tag_name\": \"${APP_VERSION}\", \"name\": \"${APP_VERSION}\"}"
-curl -H "Authorization: token ${GH_WRITE_TOKEN}" -H "Content-Type: application/json" -d "${body}" https://api.github.com/repos/${GITHUB_REPO_SLUG}/releases
+else
+    echo "Staging build successful - Creating a release in GitHub for ${GITHUB_REPO_SLUG}"
+    body="{\"tag_name\": \"${APP_VERSION}\", \"name\": \"${APP_VERSION}\"}"
+    curl -H "Authorization: token ${GH_WRITE_TOKEN}" -H "Content-Type: application/json" -d "${body}" https://api.github.com/repos/${GITHUB_REPO_SLUG}/releases
+fi
+
