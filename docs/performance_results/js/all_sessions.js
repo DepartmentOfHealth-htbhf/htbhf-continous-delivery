328f912a9983f202743b15a11002e0e975e32f53
Travis CI <travis@travis-ci.org>
2019-01-22 13:14:22 +0000

    Publishing test results

--- docs/performance_results/js/all_sessions.js
+++ docs/performance_results/js/all_sessions.js
@@ -3,7 +3,7 @@ allUsersData = {
 color: '#FF9D00',
 name: 'Active Users',
 data: [
-  [1548152873000,50],[1548152874000,50]
+  [1548162852000,50],[1548162853000,50]
 ],
 tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
     , zIndex: 20
