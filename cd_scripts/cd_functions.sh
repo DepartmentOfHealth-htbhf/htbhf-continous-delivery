73054276d45161b52cb3eab79409311424dbf5f5
Travis CI <travis@travis-ci.org>
2019-03-14 16:19:10 +0000

    NO-JIRA Added force option to unzip manifest.

--- cd_scripts/cd_functions.sh
+++ cd_scripts/cd_functions.sh
@@ -117,7 +117,7 @@ prepare_java_app_for_deploy(){
     wget -q -O artefact.jar ${APP_URL}
     wget -q -O manifest.jar ${MANIFEST_URL}
     # extract the manifest into the current directory
-    unzip manifest.jar "manifest.yml"
+    unzip -f manifest.jar "manifest.yml"
     export APP_PATH=artefact.jar
 }
 
